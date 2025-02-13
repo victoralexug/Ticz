b65f5476858dba08e07c5a4dab4f2c637f11fc82
Victor Eze <74573591+victoralexug@users.noreply.github.com>
2025-02-13 21:47:09 +0000

    gh-pages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/ticz" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/ticket-form" element={<TicketForm />} />
         <Route path="/ticket" element={<Ticket />} />
       </Routes>
